e06e16c3d97bdb74817aa301bf7ade8029ce7afc
i_anujjangra <i_anujjangra@lidolearning.com>
2022-03-04 00:30:25 +0530

    Add Changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const notes = require('./Data/notes');
+const connectDB = require('./config/db');
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("API is Running");
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
 });
 
 const PORT = process.env.PORT || 5000;
--- frontside/src/Screens/MyNotes/index.js
+++ frontside/src/Screens/MyNotes/index.js
@@ -1,67 +1,72 @@
-// import React from 'react'
-// import { Button, Card, Accordion } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
-// import MainScreen from '../../Components/MainScreen'
-// import notes from '../../Data/notes'
-// import './MyNotes.css';
+import React, {useEffect, useState} from 'react'
+import { Button, Card, Accordion } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import MainScreen from '../../Components/MainScreen'
+import './MyNotes.css';
 
-// const MyNotes = () => {
+const MyNotes = () => {
 
-//     const _deleteNote = (id) => {
-//         if (window.confirm("Are you Sure?")) {
+    const _deleteNote = (id) => {
+        if (window.confirm("Are you Sure?")) {
 
-//         }
-//     }
+        }
+    }
 
+    const [notes, setNotes] = useState([]);
 
+    const fetchNotes = async() => {
+        const {data} = await axios.get('/api/notes');
+        setNotes(data);
+    }
 
-//     return (
-//         <MainScreen title='Welcome Back Anuj...'>
-//             <Link to='/createnote'>
-//                 <Button className='buttonStyle'>Create New Note</Button>
-//             </Link>
-//             {
-//                 notes.map(note => {
-//                     return (
-//                         <Accordion>
-//                             <Card style={{ margin: '10px' }} key={note._id}>
-//                                 <Card.Header className="cardHeader">
-//                                     {/* <Accordion.Toggle as={Card.Text} eventKey='0' style={{ width: '100%' }}> */}
-//                                     <div className='note-title'>
-//                                         <Accordion.Toggle
-//                                             as={Card.Text}
-//                                             variant="link"
-//                                             eventKey="0"
-//                                         >
-//                                             {note.title}
-//                                         </Accordion.Toggle>
-//                                     </div>
-//                                     <div>
-//                                         <Button href={`/note/${note._id}`}>Edit</Button>
-//                                         <Button variant='danger' className='mx-2' onClick={_deleteNote}>Delete</Button>
-//                                     </div>
-//                                     {/* </Accordion.Toggle> */}
-//                                 </Card.Header>
-//                                 <Accordion.Collapse eventKey='0'>
-//                                     <Card.Body>
-//                                         <blockquote className="blockquote mb-0">
-//                                             <div className='note-category'>{note.category}</div>
-//                                             <p>{note.content}</p>
-//                                             <footer className="blockquote-footer">
-//                                                 Someone famous in <cite title="Source Title">Source Title</cite>
-//                                             </footer>
-//                                         </blockquote>
-//                                     </Card.Body>
-//                                 </Accordion.Collapse>
-//                             </Card>
+    useEffect(() => {
+        fetchNotes();
+    },[]);
 
-//                         </Accordion>
+    return (
+        <MainScreen title='Welcome Back Anuj...'>
+            <Link to='/createnote'>
+                <Button className='buttonStyle'>Create New Note</Button>
+            </Link>
+            {
+                notes.map(note => {
+                    return (
+                        <Accordion key={note._id}>
+                            <Card style={{ margin: '10px' }}>
+                                <Card.Header className="cardHeader">
+                                    <div className='note-title'>
+                                        <Accordion.Toggle
+                                            as={Card.Text}
+                                            variant="link"
+                                            eventKey="0"
+                                        >
+                                            {note.title}
+                                        </Accordion.Toggle>
+                                    </div>
+                                    <div>
+                                        <Button href={`/note/${note._id}`}>Edit</Button>
+                                        <Button variant='danger' className='mx-2' onClick={_deleteNote}>Delete</Button>
+                                    </div>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey='0'>
+                                    <Card.Body>
+                                        <blockquote className="blockquote mb-0">
+                                            <div className='note-category'>{note.category}</div>
+                                            <p>{note.content}</p>
+                                            <footer className="blockquote-footer">
+                                                Someone famous in <cite title="Source Title">Source Title</cite>
+                                            </footer>
+                                        </blockquote>
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    );
+                })
+            }
+        </MainScreen>
+    )
+}
 
-//                     );
-//                 })
-//             }
-//         </MainScreen>
-//     )
-// }
-
-// export default MyNotes
+export default MyNotes
